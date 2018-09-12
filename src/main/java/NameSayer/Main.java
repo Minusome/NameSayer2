72e1bd392d6512686a4b3e699bd6782387e4c9f7
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2018-09-13 04:52:43 +1200

    added new gui for names selection menu

--- src/main/java/NameSayer/Main.java
+++ src/main/java/NameSayer/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/MenuScreen.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("NameSayer");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
---
+++ src/main/java/NameSayer/MenuScreenController.java
@@ -0,0 +1,30 @@
+package NameSayer;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuScreenController implements Initializable {
+
+    @FXML
+    public JFXButton practiceButton;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void onPracticeModeClicked(MouseEvent mouseEvent) throws IOException {
+        Scene scene = practiceButton.getScene();
+        Parent root = FXMLLoader.load(getClass().getResource("/NameSelectScreen.fxml"));
+        scene.setRoot(root);
+    }
+}
