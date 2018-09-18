2059385da160cb995d02c044ff83b985cde48c64
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2018-09-19 09:18:50 +1200

    TheMicTestingFunction
    Once clicked on the Mic button a progressbar shows up indicating microphone testing in progress. And anything that the microphone detected is returned to the speaker after a short delay indicating the functionality of the microphone.
    The testing process is terminated with another click on the mic button.
    Tried to import an microphone Icon but failed, don't know the reason yet.
    That is propobaly all for this update.

--- src/main/java/namesayer/MenuScreenController.java
+++ src/main/java/namesayer/MenuScreenController.java
@@ -1,15 +1,20 @@
 package namesayer;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXProgressBar;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.DirectoryChooser;
 import namesayer.recording.NameStorageManager;
 
+import javax.sound.sampled.*;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -17,11 +22,19 @@ import java.util.ResourceBundle;
 
 public class MenuScreenController implements Initializable {
 
+    @FXML public JFXProgressBar MicrophoneVolume;
+    @FXML public JFXButton MicrophoneButton;
     @FXML private JFXButton practiceButton;
     private boolean isDirectorySelected = false;
-
+    private boolean isFirstTimeClickMic = true;
+    @FXML private JFXButton microphoneTestingButton;
+    private Thread tesetingMicThread;
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
+        MicrophoneButton.setOnMouseClicked(event -> isFirstTimeClickMic = !isFirstTimeClickMic);
+        //unale to load the icon for the microphone for some reason
+//        Image image = new Image(getClass().getResourceAsStream("src/icon/microphone2.png"));
+//        MicrophoneButton.setGraphic(new ImageView(image));
     }
 
     public void onPracticeModeClicked(MouseEvent mouseEvent) throws IOException {
@@ -30,6 +43,63 @@ public class MenuScreenController implements Initializable {
         scene.setRoot(root);
     }
 
+    //reveal the progressbar after microphone button being clicked
+    public void onMicrophoneButtonClicked(){
+        if(isFirstTimeClickMic) {
+            MicrophoneVolume.setVisible(true);
+            Thread thread = new Thread(() -> {
+                TestMicrophone();
+            });
+            thread.start();
+            tesetingMicThread = thread;
+        }else{
+            MicrophoneVolume.setVisible(false);
+                tesetingMicThread.interrupt();
+        }
+    }
+
+    private void TestMicrophone() {
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100F, 16, 2, 4,
+                44100F, true);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format); // format is an AudioFormat object //info from computer audio system
+
+        if (!AudioSystem.isLineSupported(info)) {
+            // Handle the error ...
+
+        }
+// Obtain and open the line.
+        DataLine.Info targetInfo = new DataLine.Info(TargetDataLine.class, format);
+        DataLine.Info sourceInfo = new DataLine.Info(SourceDataLine.class, format);
+
+        try {
+            TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+            targetLine.open(format);
+            targetLine.start();
+
+            SourceDataLine sourceLine = (SourceDataLine) AudioSystem.getLine(sourceInfo);
+            sourceLine.open(format);
+            sourceLine.start();
+
+            int numBytesRead;
+            byte[] targetData = new byte[targetLine.getBufferSize() / 5];
+
+            while (true) {
+                numBytesRead = targetLine.read(targetData, 0, targetData.length);
+
+                if (numBytesRead == -1)	break;
+
+                sourceLine.write(targetData, 0, numBytesRead);
+                if(isFirstTimeClickMic){
+                    sourceLine.stop();
+                    sourceLine.close();
+                }
+            }
+        }
+        catch (Exception e) {
+            System.err.println(e);
+        }
+
+    }
 
     public void onSelectAudioDatabaseFolder(MouseEvent mouseEvent) {
         DirectoryChooser chooser = new DirectoryChooser();
@@ -41,4 +111,5 @@ public class MenuScreenController implements Initializable {
             isDirectorySelected = true;
         }
     }
+
 }
